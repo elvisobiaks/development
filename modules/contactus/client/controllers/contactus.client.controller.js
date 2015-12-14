c70039246e53e66951f580c8655de6faad2613b9
Elvis Obiakara <eobiakara@metropolitan.co.za>
2015-12-14 10:12:37 +0200

    mean js play ground

--- modules/contactus/client/controllers/contactus.client.controller.js
+++ modules/contactus/client/controllers/contactus.client.controller.js
@@ -2,11 +2,21 @@
 
 // Articles controller
 angular.module('contactus').controller('ContactUsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
-	function ($scope, $stateParams, $location, Authentication, Articles) {
+	function ($scope, $stateParams, $location, Authentication, Articles, ngMap) {
 		$scope.authentication = Authentication;
 
 		var vm = this;
 		vm.resultMessage = '';
+
+		function init_map() {
+			//ngMap.getMap().then(function(map) {
+			//	console.log(map.getCenter());
+			//	console.log('markers', map.markers);
+			//	console.log('shapes', map.shapes);
+			//});
+			init_map();
+
+		}
 
 	}
 ]);
